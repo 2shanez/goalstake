80bb5e66feeacf71a7087f93d6b773daef099cb6
Ubuntu <ubuntu@ip-172-31-13-34.us-east-2.compute.internal>
2026-02-05 03:45:34 +0000

    move theme toggle to right of sign out

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -85,8 +85,8 @@ export default function Home() {
             </a>
             <StravaConnect />
             <FundWalletButton />
-            <ThemeToggle />
             <PrivyConnectButton />
+            <ThemeToggle />
           </div>
         </div>
       </header>
